237b3a8e92a82bc4f2d3f2877ca70cbd5afbd6a6
kanha sydaphasavanh <k.sydaphasavanh@gmail.com>
2019-08-21 13:30:08 +0200

    commit de style

---
+++ public/js/hideshow.js
@@ -0,0 +1,18 @@
+function hideshow() {
+  var x = document.getElementById("searchbar");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+
+function hideshow2() {
+  var nav = document.getElementById('#nav')
+  if (nav.style.display === "none") {
+    nav.style.display = "block";
+  } else {
+    nav.style.display = "none";
+  }
+}
